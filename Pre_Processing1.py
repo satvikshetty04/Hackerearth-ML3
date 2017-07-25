2388c2a649de3d655cb2cd071724de166799eb2d
satvikshetty04 <satvikshetty04@gmail.com>
2017-07-25 17:45:04 -0400

    Reading files

--- Pre_Processing1.py
+++ Pre_Processing1.py
@@ -6,4 +6,18 @@ import sys
 import datetime
 import numpy as np
 import pandas as pd
+import os
+
+start_time = datetime.datetime.now()
+print("Started at: " + start_time)
+
+# --------------------- Converters
+
+
+# --------------------- Loading dataset
+
+train = pd.read_csv("C:\\Users\\satvi\\Documents\\Projects\\Hackerearth - ML 3\\train.csv",
+                    header=0,
+                    converters={'datetime':date_conv})
+
 
