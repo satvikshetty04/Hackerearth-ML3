c49276adec6124495280b5c6e90bb344b2a4bccb
satvikshetty04 <satvikshetty04@gmail.com>
2017-07-25 21:38:51 -0400

    Feature extraction

--- Pre_Processing1.py
+++ Pre_Processing1.py
@@ -12,21 +12,30 @@ import os
 # --------------------- Initializing variables
 
 start_time = datetime.datetime.now()
-print("Started at: " + start_time)
+print("Started at: " + str(start_time))
 
-
-# --------------------- Converters
-
-
-def date_conv(x):
-    return x
 
 # --------------------- Loading dataset
 
 train = pd.read_csv("C:\\Users\\satvi\\Documents\\Projects\\Hackerearth - ML 3\\train.csv",
-                    header=0,
-                    converters={'datetime':date_conv})
+                    header=0)
+
+test = pd.read_csv("C:\\Users\\satvi\\Documents\\Projects\\Hackerearth - ML 3\\test.csv",
+                    header=0)
+
+# --------------------- Creating day, hour features
+
+train['day'] = pd.to_datetime(train['datetime']).dt.day
+train['hour'] = pd.to_datetime(train['datetime']).dt.hour
+test['day'] = pd.to_datetime(test['datetime']).dt.day
+test['hour'] = pd.to_datetime(test['datetime']).dt.hour
 
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
+print(train)
+print(test)
+
+
+# train = pd.read_csv("train.csv")
+# test = pd.read_csv("test.csv")
+
+print("Time taken: "+ str(datetime.datetime.now() - start_time))
