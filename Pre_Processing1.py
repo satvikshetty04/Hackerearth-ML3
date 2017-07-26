83b8d97f9f7191f5e4806743629f622da9bbee96
satvikshetty04 <satvikshetty04@gmail.com>
2017-07-26 00:46:16 -0400

    Imputing values

--- Pre_Processing1.py
+++ Pre_Processing1.py
@@ -17,16 +17,17 @@ print("Started at: " + str(start_time))
 
 # --------------------- Loading dataset
 
+# SS
 train = pd.read_csv("C:\\Users\\satvi\\Documents\\Projects\\Hackerearth - ML 3\\train.csv",
                     header=0)
 
 test = pd.read_csv("C:\\Users\\satvi\\Documents\\Projects\\Hackerearth - ML 3\\test.csv",
                     header=0)
 
-          # GJ
+# GJ
+# train = pd.read_csv("train.csv")
+# test = pd.read_csv("test.csv")
 
-train = pd.read_csv("train.csv")
-test = pd.read_csv("test.csv")
 
 # --------------------- Creating day, hour features
 
@@ -36,6 +37,7 @@ test['day'] = pd.to_datetime(test['datetime']).dt.day
 test['hour'] = pd.to_datetime(test['datetime']).dt.hour
 
 
+# --------------------- Replacing null values - device ID
 
 train['devid'] = np.where(train['devid'].isnull() 
                         &(~train['browserid'].isnull())
@@ -52,18 +54,31 @@ train['devid'] = np.where(train['devid'].isnull()
                         &(train['browserid'].isin(['Internet Explorer', 'Safari', 'Edge'])), 
                         'Tablet', train['devid'] )
 
+test['devid'] = np.where(test['devid'].isnull() 
+                        &(~test['browserid'].isnull())
+                        &(test['browserid'].isin(['IE', 'Google Chrome', 'Firefox', 'Opera'])), 
+                        'Mobile', test['devid'] )
 
-unique_id = list(set(train[train['devid'].isnull() & train['browserid'].isnull()]['offerid']))
-unique_id = sorted(unique_id)
+test['devid'] = np.where(test['devid'].isnull() 
+                        &(~test['browserid'].isnull())
+                        &(test['browserid'].isin(['InternetExplorer', 'Mozilla Firefox', 'Mozilla', 'Chrome'])), 
+                        'Desktop', test['devid'] )
+
+test['devid'] = np.where(test['devid'].isnull() 
+                        &(~test['browserid'].isnull())
+                        &(test['browserid'].isin(['Internet Explorer', 'Safari', 'Edge'])), 
+                        'Tablet', test['devid'] )
+
+unique_offerid = list(set(train[train['devid'].isnull() & train['browserid'].isnull()]['offerid']))
+unique_offerid = sorted(unique_offerid)
+
+train.isnull().sum(axis=0)/train.shape[0]
+        
+for i in unique_offerid:
+    print(train[train['offerid']==i]['devid'].mode())
 
 print(train)
 print(test)
 
-
-
-train.isnull().sum(axis=0)/train.shape[0]
-        
-print([train[train['offerid']==i]['devid'].mode() for i in unique_id])
-
-print("Time taken: "+ str(datetime.datetime.now() - start_time))
+print("Time taken: " + str(datetime.datetime.now() - start_time))
 
