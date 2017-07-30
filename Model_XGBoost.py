aac54abfe2ae4be311c290f2f7789468dfa3b99d
satvikshetty04 <satvikshetty04@gmail.com>
2017-07-29 20:54:48 -0400

    Removed siteid, min, weekday

--- Model_XGBoost.py
+++ Model_XGBoost.py
@@ -7,6 +7,7 @@ import datetime
 import numpy as np
 import pandas as pd
 from xgboost.sklearn import XGBClassifier
+from sklearn.model_selection import train_test_split
 
 # --------------------- Initializing variables
 
@@ -25,6 +26,7 @@ test = pd.read_csv("Data\\test_pp1.csv", header=0)
 train['siteid'].fillna(-999, inplace=True)
 test['siteid'].fillna(-999, inplace=True)
 
+
 # --------------------- Dropping columns
 
 pred_df['ID'] = test['ID']
@@ -33,19 +35,29 @@ train.drop(cols_to_drop, axis=1, inplace=True)
 test.drop(cols_to_drop, axis=1, inplace=True)
 
 
+train.drop(['weekday','minute','siteid'], axis = 1, inplace=True)
+test.drop(['weekday','minute','siteid'], axis = 1, inplace=True)
+
 # --------------------- Splitting data
 
-Y = train['click'].as_matrix()
+Y = train['click']
 train.drop('click', axis=1, inplace=True)
-X = train.as_matrix()
-test = test.as_matrix()
-
+X = train
+# test = test.as_matrix()
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.3, random_state=125)
+# X_train = X_train.as_matrix()
+# X_test = X_test.as_matrix()
+# y_train = y_train.as_matrix()
+# y_test = y_test.as_matrix()
 
 # --------------------- Creating model
 
-xgbclassifier = XGBClassifier(n_estimators=100, nthread=-1, silent=False, seed=125)
+xgbclassifier = XGBClassifier(n_estimators=100, nthread=-1, silent=False, seed=125, learning_rate=0.2)
+# xgbmodel = xgbclassifier.fit(X_train, y_train)
 xgbmodel = xgbclassifier.fit(X, Y)
+
 # pred = xgbmodel.predict(test)
+xgbmodel.score(X_test, y_test)
 pred = xgbmodel.predict_proba(test)[:,1]
 
 
@@ -53,5 +65,5 @@ pred = xgbmodel.predict_proba(test)[:,1]
 
 pred_df['click'] = pred
 file_name = "Predictions\\prediction_" + str(datetime.datetime.now().date()) + "_" +\
-            str(datetime.datetime.now().strftime("%H:%M:%S")) + ".csv"
-pred_df.to_csv(path_or_buf="Predictions\\prediction_4.csv", index=False)
+            str(datetime.datetime.now().strftime("%H%M%S")) + ".csv"
+pred_df.to_csv(path_or_buf="Predictions\\prediction_8.csv", index=False)
