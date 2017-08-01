5a2b4893e444c972a0b2b6a9c4821ea21b861c97
satvikshetty04 <satvikshetty04@gmail.com>
2017-07-31 23:45:30 -0400

    Updated model

--- Model_XGBoost.py
+++ Model_XGBoost.py
@@ -26,17 +26,15 @@ test = pd.read_csv("Data\\test_pp1.csv", header=0)
 train['siteid'].fillna(-999, inplace=True)
 test['siteid'].fillna(-999, inplace=True)
 
+train['hour_range'] = np.where(train['hour'].isin([0,1,20,21,22,23]), 1, 0)
+test['hour_range'] = np.where(test['hour'].isin([0,1,20,21,22,23]), 1, 0)
 
 # --------------------- Dropping columns
 
 pred_df['ID'] = test['ID']
-cols_to_drop = ['ID', 'datetime']
+cols_to_drop = ['ID', 'datetime', 'weekday','minute','siteid']
 train.drop(cols_to_drop, axis=1, inplace=True)
 test.drop(cols_to_drop, axis=1, inplace=True)
-
-
-train.drop(['weekday','minute','siteid'], axis = 1, inplace=True)
-test.drop(['weekday','minute','siteid'], axis = 1, inplace=True)
 
 # --------------------- Splitting data
 
@@ -66,4 +64,4 @@ pred = xgbmodel.predict_proba(test)[:,1]
 pred_df['click'] = pred
 file_name = "Predictions\\prediction_" + str(datetime.datetime.now().date()) + "_" +\
             str(datetime.datetime.now().strftime("%H%M%S")) + ".csv"
-pred_df.to_csv(path_or_buf="Predictions\\prediction_8.csv", index=False)
+pred_df.to_csv(path_or_buf="Predictions\\prediction_9.csv", index=False)
